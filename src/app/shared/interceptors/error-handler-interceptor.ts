e6f0d9d1f35a27d7eb88c89f57104b72bedb003e
Erjetto <winata_rheza@yahoo.com>
2020-09-06 13:06:10 +0700

    Added interceptor for request handling

---
+++ src/app/shared/interceptors/error-handler-interceptor.ts
@@ -0,0 +1,30 @@
+import {
+	HttpInterceptor,
+	HttpHandler,
+	HttpRequest,
+	HttpEvent,
+	HttpResponse,
+  HttpErrorResponse,
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { tap, catchError } from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+import { IAppState } from 'src/app/app.reducer';
+import { Store } from '@ngrx/store';
+import { MainStateAction } from '../store-modules';
+
+@Injectable({
+	providedIn: 'root',
+})
+export class ErrorHandlerInterceptor implements HttpInterceptor {
+	constructor(private store: Store<IAppState>) {}
+
+	intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+		return next.handle(req).pipe(
+      catchError((error: HttpErrorResponse) => {
+        this.store.dispatch(MainStateAction.FailMessage(error.message));
+        return throwError(error)
+      })
+		);
+	}
+}
