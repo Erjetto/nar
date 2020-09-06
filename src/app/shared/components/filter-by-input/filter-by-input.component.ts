e1c8f7e09fa396146cd8e8f83c6f195ecc43fb5d
Erjetto <winata_rheza@yahoo.com>
2020-09-06 13:08:55 +0700

    Added component to filter the input

---
+++ src/app/shared/components/filter-by-input/filter-by-input.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'rd-filter-by-input',
+  templateUrl: './filter-by-input.component.html',
+  styleUrls: ['./filter-by-input.component.scss']
+})
+export class FilterByInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -18,12 +18,13 @@ import { ClockComponent } from './components/clock/clock.component';
 import { CardComponent } from './components/card/card.component';
 import { ToasterComponent } from './components/toaster/toaster.component';
 import { ModalComponent } from './components/modal/modal.component';
+import { FilterByInputComponent } from './components/filter-by-input/filter-by-input.component';
 
 import { ClickStopPropagationDirective } from './directives/click-stop-propagation.directive';
 import { ConfirmableButtonComponent } from './components/confirmable-button/confirmable-button.component';
 import { ContrastTextColorDirective } from './directives/contrast-text-color.directive';
 import { EasyHorizontalSrollDirective } from './directives/easy-horizontal-sroll.directive';
-import { PresentationQuestionComponent } from './component/presentation-question/presentation-question.component';
+import { PresentationQuestionComponent } from './components/presentation-question/presentation-question.component';
 import { VarDirective } from './directives/var.directive';
 
 const SHARED_DECLARATIONS = [
@@ -34,7 +35,8 @@ const SHARED_DECLARATIONS = [
   ModalComponent, 
   ConfirmableButtonComponent,
   PresentationQuestionComponent,
-
+  FilterByInputComponent,
+  
   ClickStopPropagationDirective,
   ContrastTextColorDirective,
   EasyHorizontalSrollDirective,
@@ -42,7 +44,7 @@ const SHARED_DECLARATIONS = [
 ];
 
 @NgModule({
-	declarations: [...SHARED_DECLARATIONS,  ],
+	declarations: [...SHARED_DECLARATIONS,   ],
 	imports: [
 		FontAwesomeModule,
 		CommonModule,
