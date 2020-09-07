36e442e5de87d74210840a6ae9226a961f548ce7
Erjetto <winata_rheza@yahoo.com>
2020-09-07 11:07:02 +0700

    Added after request to force afterRequest$

--- src/app/shared/interceptors/after-request-interceptor.ts
+++ src/app/shared/interceptors/after-request-interceptor.ts
@@ -4,10 +4,10 @@ import {
 	HttpRequest,
 	HttpEvent,
 	HttpResponse,
-  HttpErrorResponse,
+	HttpErrorResponse,
 } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import { tap, catchError } from 'rxjs/operators';
+import { tap, catchError, finalize } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
 import { IAppState } from 'src/app/app.reducer';
 import { Store } from '@ngrx/store';
@@ -16,16 +16,12 @@ import { MainStateAction } from '../store-modules';
 @Injectable({
 	providedIn: 'root',
 })
-export class ErrorHandlerInterceptor implements HttpInterceptor {
+export class AfterRequestInterceptor implements HttpInterceptor {
 	constructor(private store: Store<IAppState>) {}
 
 	intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-		return next.handle(req).pipe(
-      catchError((error: HttpErrorResponse) => {
-        // this.store.dispatch(MainStateAction.FailMessage(error.message));
-        this.store.dispatch(MainStateAction.RequestFailedMessage(error));
-        return throwError(error)
-      })
-		);
+		return next
+			.handle(req)
+			.pipe(finalize(() => this.store.dispatch(MainStateAction.AfterRequest())));
 	}
 }
