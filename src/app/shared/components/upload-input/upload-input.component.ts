179ac3de75fa769dcc1ac0020da92fdacb3b9a01
Erjetto <winata_rheza@yahoo.com>
2020-11-02 03:13:11 +0700

    Improved upload input

--- src/app/shared/components/upload-input/upload-input.component.ts
+++ src/app/shared/components/upload-input/upload-input.component.ts
@@ -1,7 +1,12 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Component, OnInit, Input, Output, EventEmitter, HostBinding } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { IAppState } from 'src/app/app.reducer';
+import { OtherService } from '../../services/new/other.service';
 import { MainStateEffects, MainStateAction } from '../../store-modules';
+import { flatten as _flatten, isEmpty as _isEmpty } from 'lodash';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
+import { adjustControlsInFormArray } from '../../methods';
 
 /*
 NOTE:
@@ -15,21 +20,49 @@ NOTE:
 	styleUrls: ['./upload-input.component.scss'],
 })
 export class UploadInputComponent implements OnInit {
-	@Input() files: string[];
-	@Input() multiple = false;
-  @Output() upload = new EventEmitter<string>();
+	@HostBinding('class') hostClass = 'col input-group';
 
-	constructor(protected store: Store<IAppState>, private mainEffects: MainStateEffects) {}
+  /**
+   * FormArray will update fileId & fileName
+   */
+	@Input() filesForm: FormArray = new FormArray([]);
+  @Input() multiple = false;
+  fileNamesArr = ''
+
+	constructor(protected store: Store<IAppState>, private otherService: OtherService) {}
 
 	ngOnInit(): void {}
 
 	get hasFile() {
-		return this.files.length > 0;
+		return this.filesForm.length > 0;
 	}
 
 	uploadFile(files: FileList) {
-		this.store.dispatch(MainStateAction.UploadFile({ files: { ...files, length: files.length } }));
-  }
+    if(files.length === 0) return;
+		// this.store.dispatch(MainStateAction.UploadFile({ files: { ...files, length: files.length } }));
+    this.store.dispatch(MainStateAction.InfoMessage('Uploading files...'));
+    
+		this.otherService.UploadFiles(files).subscribe((res) => {
+			const fileIdsArr: string[] = _flatten([res.fileid]);
+      const fileNamesArr: string[] = _flatten([res.filename]);
+      this.fileNamesArr = fileNamesArr.join(', ');
+
+			this.store.dispatch(
+				MainStateAction.SuccessfullyMessage('uploaded file(s) : ' + fileNamesArr.join(', '))
+      );
+      const fileFormFactory = () => ({
+        fileName: new FormControl(''), 
+        fileId: new FormControl('')
+      })
+      adjustControlsInFormArray(this.filesForm, fileIdsArr.length, fileFormFactory)
+      this.filesForm.patchValue(
+        Array(fileIdsArr.length).fill(0).map((v, idx) => ({ 
+          fileId: fileIdsArr[0], 
+          fileName: fileNamesArr[1] 
+        }))
+      )
+		});
+	}
 
 	removeUploadedFiles(element) {
 		element.target.value = '';
