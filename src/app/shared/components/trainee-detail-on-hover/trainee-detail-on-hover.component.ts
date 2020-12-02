a1836c57ff0b06178c1b8c5a9d70725e3f342927
Erjetto <winata_rheza@yahoo.com>
2020-12-03 02:55:42 +0700

    Added hover effect to show trainee

---
+++ src/app/shared/components/trainee-detail-on-hover/trainee-detail-on-hover.component.ts
@@ -0,0 +1,68 @@
+import {
+	Component,
+	OnInit,
+	Output,
+	EventEmitter,
+	Input,
+	HostBinding,
+	ElementRef,
+	HostListener,
+	ViewChild,
+	TemplateRef,
+	OnDestroy,
+  AfterViewInit,
+  ChangeDetectionStrategy,
+} from '@angular/core';
+import { ColorHelper } from '../../utilities/color-helper';
+import { NgbPopover } from '@ng-bootstrap/ng-bootstrap';
+import { isString as _isString } from 'lodash';
+import { ClientTrainee, ClientTraineeData } from '../../models';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { map, takeUntil, withLatestFrom } from 'rxjs/operators';
+import { IAppState } from 'src/app/app.reducer';
+import { select, Store } from '@ngrx/store';
+import { fromBinusianState, fromNoteState, NoteStateAction } from '../../store-modules';
+
+@Component({
+	selector: 'rd-trainee-detail-on-hover',
+	templateUrl: './trainee-detail-on-hover.component.html',
+  styleUrls: ['./trainee-detail-on-hover.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TraineeDetailOnHoverComponent implements OnInit, OnDestroy, AfterViewInit  {
+  viewDateFormat = 'dd MMM yyyy, HH:mm:ss';
+  
+	@ViewChild('pop') popOver: NgbPopover;
+	@Input() autoClose: boolean | string = 'outside';
+  @Input() traineeId = '725bea16-ad0e-eb11-abcb-d8d385fcda38';
+  @Input() numberOfComments = 7; // Jumlah comment yg dimunculkan
+  
+	destroyed$: Subject<void> = new Subject<void>();
+	traineeDetail$: Observable<ClientTraineeData>;
+	loadingViewTrainee$ = new BehaviorSubject<boolean>(false);
+
+	constructor(private store: Store<IAppState>) {}
+
+  ngOnInit(){}
+
+	ngAfterViewInit(): void {
+		this.traineeDetail$ = this.store.pipe(
+      select(fromNoteState.getCurrentTraineeDetail)
+    );
+		this.store
+			.pipe(select(fromNoteState.isLoadingCurrentTraineeDetail))
+			.pipe(takeUntil(this.destroyed$))
+			.subscribe(this.loadingViewTrainee$);
+
+		this.popOver.shown.pipe(takeUntil(this.destroyed$)).subscribe(() => {
+			this.store.dispatch(
+				NoteStateAction.FetchTraineeDataForTrainer({ traineeId: this.traineeId })
+			);
+		});
+	}
+
+	ngOnDestroy(): void {
+		this.destroyed$.next();
+		this.destroyed$.complete();
+	}
+}
