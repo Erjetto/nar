54211260b9117d6e1b3a7b87f11cbdc5e5cd1e4e
Erjetto <winata_rheza@yahoo.com>
2021-03-12 23:12:03 +0700

    Added new service

--- src/app/shared/services/new/rest.service.ts
+++ src/app/shared/services/new/rest.service.ts
@@ -4,6 +4,8 @@ import { isEmpty as _isEmpty, map as _map } from 'lodash';
 import { environment } from 'src/environments/environment';
 import { TraineeSchedule } from '../../models';
 import { delay, map } from 'rxjs/operators';
+import { DateHelper } from '../../utilities/date-helper';
+import { Observable } from 'rxjs';
 
 @Injectable({
 	providedIn: 'root',
@@ -13,7 +15,7 @@ export class RESTService {
 	constructor(protected httpClient: HttpClient) {}
 
 	/**
-	 * Join url segments in order, while also cutting it if some param is null, ex:
+	 * Join url segments in order, and cut it if null param is found, ex:
 	 * - `[genId, traineeId, date, type]`
 	 * - `date` is null
 	 * - Result: `genId/traineeId`
@@ -32,10 +34,34 @@ export class RESTService {
 		type: string = null,
 		date: string = null,
 		traineeScheduleId: string = null
-	) {
+	) : Observable<TraineeSchedule[]> {
 		const params = this.joinLinkInOrder([generationId, traineeId, type, date, traineeScheduleId]);
 		return this.httpClient
-			.get(`${this.baseUrl}TraineeSchedule/${params}`)
-			.pipe(map((res: any) => _map(res.d, TraineeSchedule.fromJson)));
+			.get(`${this.baseUrl}TraineeSchedule/${params}/`)
+			.pipe(map((res: any) => _map(res, TraineeSchedule.fromJson)));
+	}
+
+	public CreateTraineeSchedule(data: TraineeSchedule): Observable<boolean> {
+		return this.httpClient
+			.post(`${this.baseUrl}TraineeSchedule/`, {data:{
+				...data,
+				AttendanceDate: DateHelper.toCSharpDate(data.AttendanceDate)
+			}})
+			.pipe(map((res: any) => res === true));
+	}
+
+	public DeleteTraineeSchedule(traineeScheduleId: string) : Observable<boolean> {
+		return this.httpClient
+			.delete(`${this.baseUrl}TraineeSchedule/`)
+			.pipe(map((res: any) => res === true));
+	}
+	
+	public UpdateTraineeSchedule(data: TraineeSchedule): Observable<boolean> {
+		return this.httpClient
+			.put(`${this.baseUrl}TraineeSchedule/`, {data:{
+				...data,
+				AttendanceDate: DateHelper.toCSharpDate(data.AttendanceDate)
+			}})
+			.pipe(map((res: any) => res === true));
 	}
 }
--- src/app/shared/services/new/trainer-attendance.service.ts
+++ src/app/shared/services/new/trainer-attendance.service.ts
@@ -1,9 +1,107 @@
 import { Injectable } from '@angular/core';
+import { Observable, of, throwError } from 'rxjs';
+import { delay, map } from 'rxjs/operators';
+import {
+	ClientTrainerTeachingSchedule,
+	ClientTrainerTeachingScheduleUpdateResult,
+	TrainerTeachingSchedule,
+} from '../../models';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { map as _map } from 'lodash';
+import { DateHelper } from '../../utilities/date-helper';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root',
 })
 export class TrainerAttendanceService {
+	private baseUrl = environment.apiUrl + 'Trainee.svc/';
+	constructor(protected httpClient: HttpClient) {}
 
-  constructor() { }
+	public GetCurrentUserTeachingSchedule(): Observable<ClientTrainerTeachingSchedule[]> {
+		return this.httpClient
+			.post(this.baseUrl + 'GetCurrentUserTeachingSchedule', {})
+			.pipe(map((res: any) => _map(res.d, ClientTrainerTeachingSchedule.fromJson)));
+	}
+
+	public SaveTrainerTeachingAttendances(data: { date: string }): Observable<boolean> {
+		return this.httpClient
+			.post(this.baseUrl + 'SaveTrainerTeachingAttendances', data)
+			.pipe(map((res: any) => res.d === true));
+	}
+
+	public ChangeLecturerAttendanceStatus(data: {
+		trainerTeachingScheduleId: string;
+		note: string;
+		status: string;
+	}): Observable<boolean> {
+		return this.httpClient
+			.post(this.baseUrl + 'ChangeLecturerAttendanceStatus', data)
+			.pipe(map((res: any) => res.d === true));
+	}
+
+	public ChangeSubstituteLecturersAttendanceStatus(data: {
+		trainerTeachingScheduleId: string;
+		note: string;
+		status: string;
+	}): Observable<boolean> {
+		return this.httpClient
+			.post(this.baseUrl + 'ChangeSubstituteLecturersAttendanceStatus', data)
+			.pipe(map((res: any) => res.d === true));
+	}
+
+	public ChangeLecturerAttendanceStatusWithSubstitution(data: {
+		trainerTeachingScheduleId: string;
+		note: string;
+		username: string;
+	}): Observable<boolean> {
+		return this.httpClient
+			.post(this.baseUrl + 'ChangeLecturerAttendanceStatusWithSubstitution', data)
+			.pipe(map((res: any) => res.d === true));
+	}
+
+	public GetTrainerTeachingSchedules(data: {
+    startDate: string;
+    endDate: string;
+	}): Observable<TrainerTeachingSchedule[]> {
+		return this.httpClient
+			.post(this.baseUrl + 'GetTrainerTeachingSchedules', data)
+			.pipe(map((res: any) => _map(res.d, TrainerTeachingSchedule.fromJson)));
+	}
+
+	public GetTrainerTeachingDates(): Observable<Date[]> {
+		return this.httpClient
+			.post(this.baseUrl + 'GetTrainerTeachingDates', {})
+			.pipe(map((res: any) => res.d.map((date) => DateHelper.fromCSharpDate(date))));
+	}
+
+	public SaveLecturerTeachingSchedules(data: { schedules: string[] }): Observable<string[]> {
+		return this.httpClient
+			.post(this.baseUrl + 'SaveLecturerTeachingSchedules', data)
+			.pipe(map((res: any) => res.d as string[]));
+	}
+
+	public UpdateLecturerTeachingSchedule(data: {
+		schedule: string;
+	}): Observable<ClientTrainerTeachingScheduleUpdateResult> {
+		return this.httpClient
+			.post(this.baseUrl + 'UpdateLecturerTeachingSchedule', data)
+			.pipe(map((res: any) => ClientTrainerTeachingScheduleUpdateResult.fromJson(res.d)));
+	}
+
+	public DeleteLecturerTeachingSchedule(data: {
+		trainerTeachingScheduleId: string;
+	}): Observable<boolean> {
+		return this.httpClient
+			.post(this.baseUrl + 'DeleteLecturerTeachingSchedule', data)
+			.pipe(map((res: any) => res.d === true));
+	}
+
+	// public GetTrainerTeachingAttendances(data: {
+	// 	date: string;
+	// }): Observable<ClientTrainerTeachingAttendance> {
+	// 	return this.httpClient
+	// 		.post(this.baseUrl + 'GetTrainerTeachingAttendances', data)
+	// 		.pipe(map((res: any) => ClientTrainerTeachingAttendance.fromJson(res.d)));
+	// }
 }
