4948b40fa6989d6d20f97af4304b3c394f7558b2
Erjetto <winata_rheza@yahoo.com>
2021-06-22 17:31:03 +0700

    Fixed print interview's semester not up-to-date

--- src/app/print/print-interview-result/print-interview-result.component.ts
+++ src/app/print/print-interview-result/print-interview-result.component.ts
@@ -2,12 +2,21 @@ import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/
 import { Title } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 import { select, Store } from '@ngrx/store';
+import { isEmpty as _isEmpty } from 'lodash';
 import { BehaviorSubject, Observable, Subject } from 'rxjs';
-import { map, takeUntil } from 'rxjs/operators';
+import { filter, map, takeUntil, tap } from 'rxjs/operators';
 import { IAppState } from 'src/app/app.reducer';
-import { ClientInterviewResult } from 'src/app/shared/models';
-import { fromInterviewState, InterviewStateAction } from 'src/app/shared/store-modules';
+import { ClientGeneration, ClientInterviewResult } from 'src/app/shared/models';
+import {
+	fromInterviewState,
+	fromMainState,
+	fromMasterState,
+	InterviewStateAction,
+	MainStateAction,
+	MasterStateAction,
+} from 'src/app/shared/store-modules';
 import { DateHelper } from 'src/app/shared/utilities/date-helper';
+import { ID_Dict } from 'src/app/shared/utilities/id-lang-helper';
 
 @Component({
 	selector: 'rd-print-interview-result',
@@ -16,9 +25,13 @@ import { DateHelper } from 'src/app/shared/utilities/date-helper';
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PrintInterviewResultComponent implements OnInit, OnDestroy {
-  interviewDateFormat = DateHelper.WEEKDAY_DATE_FORMAT;
-  today = new Date()
+	interviewDateFormat = DateHelper.WEEKDAY_DATE_FORMAT;
+	today = new Date();
+	idDict = ID_Dict;
 
+	currentGeneration$ = new BehaviorSubject<ClientGeneration>(null);
+	currentYear$: Observable<string>;
+	currentSem$: Observable<string>;
 	interviewResultData$: Observable<ClientInterviewResult>;
 	total$: Observable<number>;
 
@@ -35,12 +48,31 @@ export class PrintInterviewResultComponent implements OnInit, OnDestroy {
 		this.interviewResultData$ = this.store.pipe(select(fromInterviewState.getInterviewResult));
 
 		this.total$ = this.interviewResultData$.pipe(
+			filter((v) => !_isEmpty(v)),
 			map((res) => res.Questions.reduce((prev, curr, idx) => prev + curr.Value * curr.Weight, 0))
+		);
+
+		this.store
+			.pipe(
+				select(fromMasterState.getGenerations),
+				takeUntil(this.destroyed$),
+				map((gens) => gens[0])
+			)
+			.subscribe(this.currentGeneration$);
+
+		this.currentYear$ = this.currentGeneration$.pipe(
+			filter((v) => !_isEmpty(v)),
+			map((g) => g.yearRange)
+		);
+		this.currentSem$ = this.currentGeneration$.pipe(
+			filter((v) => !_isEmpty(v)),
+			map((g) => g.Semester)
 		);
 
 		this.activatedRoute.params.subscribe(({ interviewScheduleId }) => {
 			this.store.dispatch(InterviewStateAction.FetchInterviewResult({ interviewScheduleId }));
 		});
+		this.store.dispatch(MasterStateAction.FetchGenerations());
 	}
 	ngOnDestroy(): void {
 		this.destroyed$.next();
---
+++ src/app/shared/utilities/id-lang-helper.ts
@@ -0,0 +1,7 @@
+
+// tslint:disable-next-line: object-literal-key-quotes
+// tslint:disable-next-line: variable-name
+export const ID_Dict = {
+	'Odd': 'Ganjil',
+	'Even': 'Genap',
+}
